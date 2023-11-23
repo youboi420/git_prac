5a5a2cf2c52e4da4180ef23ef5af8bdb3ec9d338
youboi420 <lolim121212@gmail.com>
2023-11-23 13:01:24 +0200

    updated-feature-1-even-more this time i mention the last update :)

--- f1.py
+++ f1.py
@@ -4,3 +4,8 @@ def moretest():
     return "this is an update to func1"
 
 print("in order to update please review")
+
+def moretest_2():
+    print("reviewed the last one")
+    print("now review this please...")
+    return "moretest2"
